d9283073a9837d4a7c5b2290479dc72c8292fe08
procrastinator01 <127467812+procrastinator01@users.noreply.github.com>
2023-03-17 13:20:32 +0900

    Update note.c
    I added the introduction of my name

--- note.c
+++ note.c
@@ -1,9 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+//touched by my second account
 
 int main()
 {
 	printf("Hello World\n");
-
+	printf("My name is Heeseo Lee\n");
 	return 0;
 }
